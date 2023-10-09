6de18af6f4c290517ec0a0dd37823f7a4dc71256
UnrealOG <creepercraft137@gmail.com>
2023-10-09 13:04:31 -0400

    setup gh-pages
    Copy SolidJS stuff over

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import styles from './App.module.css';
+import { createSignal, createEffect } from 'solid-js';
+import {Router, Route, Routes, A} from "@solidjs/router"
+import Homepage from './pages/homepage';
+import Signup from './pages/signup';
+import Login from './pages/login';
+import Deliverables from './pages/deliverables';
+
+function App() {
+  //for fetches that happen multiple times, use createResource
+  const [data, setData] = createSignal(null)
+
+  createEffect(async() => {
+      // let req = await fetch(`http://localhost:4000/api`)
+      // let data = await req.json()
+      // setData(data)
+  })
+
+  return (
+    <Router>
+    <div class={styles.App}>
+      <header class={styles.header}>
+        <A class={styles.link} href="/">Home</A>
+        Senior Design Deliverables 
+      </header>
+      {/* <p>{data()}</p> */}
+      
+      <Routes>
+        <Route path="/" component={Homepage} />
+        <Route path="/deliverables" component={Deliverables} />
+      </Routes>
+    </div>
+    </Router>
+
+  );
+}
+
+export default App;
---
+++ validation.js
@@ -0,0 +1,42 @@
+import { ObjectId } from "mongodb"
+import xss from "xss"
+
+function checkUsername(username){
+    if(!username) throw "No username provided"
+    if(typeof username!=='string') throw "Username must be a string"
+    username=xss(username.trim())
+    if(username.length<3) throw "Username must be at least 3 characters long"
+
+    return username
+}
+
+function checkPassword(password){
+    if(!password) throw "No password provided"
+    if(typeof password!=='string') throw "Password must be a string"
+    password=xss(password.trim())
+    if(password.length<3) throw "Password must be at least 3 characters long"
+
+    return password
+}
+
+function checkDisplayName(displayName){
+    if(!displayName) throw "No displayName provided"
+    if(typeof displayName!=='string') throw "displayName must be a string"
+    displayName=xss(displayName.trim())
+    if(displayName.length<3) throw "displayName must be at least 3 characters long"
+
+    return displayName
+}
+
+function checkId(id){
+    if(!id) throw "No ObjectId given"
+    if(!ObjectId.isValid(id)) throw `${id} is not a valid ObjectId`
+    return id
+}
+
+export default {
+    checkUsername,
+    checkPassword,
+    checkDisplayName,
+    checkId
+}
